73d7a8a1c0568452859b2fc8377faf9e17c99920
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-10-10 18:37:33 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lecture7TB_Labels_Buttons_RTB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        // Hotkeys
+        // F7 goes to code view
+        // Shift + F7 goes to design view
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
